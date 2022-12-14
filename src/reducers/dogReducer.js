cff54609decb3b62b9b46ff3475823aec3321d07
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-14 14:54:38 -0300

    create reducer

---
+++ src/reducers/dogReducer.js
@@ -0,0 +1,43 @@
+import {
+  GET_DOG_FAIL,
+  GET_DOG_STARTED,
+  GET_DOG_SUCCESS,
+} from '../actionTypes/actionTypes';
+
+const initialState = {
+  loading: false,
+  dogBreeds: [],
+  error: null,
+};
+
+const dogReducer = (state = initialState, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case GET_DOG_STARTED:
+      return {
+        ...state,
+        loading: true,
+      };
+
+    case GET_DOG_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        error: null,
+        dogBreeds: [...state.dogBreeds, payload],
+      };
+
+    case GET_DOG_FAIL:
+      return {
+        ...state,
+        loading: false,
+        error: payload.error,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default dogReducer;
