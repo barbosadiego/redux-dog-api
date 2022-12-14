5488e197684360c14679434c411b8a3c911536e2
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-14 14:55:43 -0300

    add initial redux in card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getDog } from '../actionCreators/actionCreators';
 
 const Card = () => {
+  const store = useSelector((store) => store);
+  const dispatch = useDispatch();
+  console.log(store);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(getDog());
+  };
+
   return (
     <>
       <h1>Select the dog breed</h1>
@@ -11,7 +22,9 @@ const Card = () => {
           <option value="ex2">ex2</option>
         </select>
         <div>
-          <button type="submit">Submit</button>
+          <button type="submit" onClick={handleSubmit}>
+            Submit
+          </button>
         </div>
       </form>
     </>
