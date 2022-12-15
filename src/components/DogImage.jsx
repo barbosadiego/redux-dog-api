5db21e17641ec7ddbfd8bcf7cb95d3a523601569
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-14 21:58:15 -0300

    update dog image component

--- src/components/DogImage.jsx
+++ src/components/DogImage.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import styled from 'styled-components';
 
-const DogImage = (image) => {
+const DogImage = ({ index }) => {
+  const store = useSelector((store) => store);
+  const arrayStore = store.dogBreeds;
+
   return (
-    <div>
-      <img src={`https://cdn2.thedogapi.com/images/${image.img}.jpg`} alt="" />
-    </div>
+    <StyledImage>
+      <img src={arrayStore[index].image.url} alt={arrayStore[index].name} />
+      <p>Name: {arrayStore[index].name}</p>
+      <p>Temperament: {arrayStore[index].temperament}</p>
+      <p>Weigth (Kg): {arrayStore[index].weight.metric}</p>
+      <p>Height (cm): {arrayStore[index].height.metric}</p>
+      <p>Life span: {arrayStore[index].life_span}</p>
+    </StyledImage>
   );
 };
 
 export default DogImage;
+
+const StyledImage = styled.div`
+  width: 500px;
+  height: 500px;
+
+  img {
+    max-width: 100%;
+    display: block;
+    object-fit: cover;
+  }
+`;
