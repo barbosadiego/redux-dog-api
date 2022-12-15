21417fa082245cef69fdb7145c75c7996c3ec25d
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-15 09:14:02 -0300

    update card style

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getDog } from '../actionCreators/actionCreators';
 import DogImage from './DogImage';
+import styled from 'styled-components';
 
 const Card = () => {
   const store = useSelector((store) => store);
@@ -22,8 +23,10 @@ const Card = () => {
   if (store.dogBreeds.length === 0) return <h1>Loading data...</h1>;
 
   return (
-    <>
-      <h1>Select the dog breed</h1>
+    <StyledCard>
+      <h1>
+        the<span>dog</span>api
+      </h1>
       <form>
         <label htmlFor="dogs">Choose a dog:</label>
         <select ref={refSelect} name="dogs" id="dogs">
@@ -33,15 +36,69 @@ const Card = () => {
             </option>
           ))}
         </select>
-        <div>
-          <button type="submit" onClick={handleSubmit}>
-            Submit
-          </button>
-        </div>
+        <button type="submit" onClick={handleSubmit}>
+          Submit
+        </button>
       </form>
       <DogImage index={index} />
-    </>
+    </StyledCard>
   );
 };
 
 export default Card;
+
+const StyledCard = styled.section`
+  box-sizing: border-box;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+  padding: 30px;
+
+  * {
+    font-family: 'Fira Sans', sans-serif;
+  }
+
+  h1 {
+    font-size: clamp(2.5rem, 10vw, 5rem);
+    margin-bottom: 0;
+    text-transform: uppercase;
+    color: grey;
+
+    span {
+      color: black;
+    }
+  }
+
+  form {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    gap: 0.5rem;
+  }
+
+  select {
+    padding: 0.5rem 1rem;
+    outline: none;
+    margin-inline: 1rem;
+  }
+
+  button {
+    padding: 0.5rem 1rem;
+    background-color: grey;
+    border: none;
+    cursor: pointer;
+    color: white;
+    border-radius: 3px;
+    transition: all 0.1s;
+    font-family: inherit;
+    text-transform: uppercase;
+
+    &:active {
+      box-shadow: 0 0 0 4px rgba(0, 0, 0, 0.3);
+    }
+  }
+`;
